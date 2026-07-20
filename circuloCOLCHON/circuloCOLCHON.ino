5b89c5493325535a9bb6536121f8a9285be2c5e7
Gastón Martino <gaston.martino@gmail.com>
2026-07-20 14:02:44 -0300

    Primera versión del código Arduino para el cirçulo "COLCHÓN"

---
+++ circuloCOLCHON/circuloCOLCHON.ino
@@ -0,0 +1,133 @@
+/**
+ * Círculo: "EL COLCHÓN DE AIRE"
+ * -----------------------------
+ * Código para activar las bombas de aire que inflan las bolsas
+ * tubulares y también las bombas que succionan el aire de las
+ * bolsas para vaciarlas.
+ */
+
+#define PIN_INFLADOR_01 8
+#define PIN_INFLADOR_02 10
+#define PIN_INFLADOR_03 12
+#define PIN_INFLADOR_04 13
+
+#define PIN_ASPIRADOR_01 7
+#define PIN_ASPIRADOR_02 6
+#define PIN_ASPIRADOR_03 5
+#define PIN_ASPIRADOR_04 4
+
+
+#define MIN_PAUSA 1000
+#define MAX_PAUSA 4000
+#define MIN_INFLACION 3000
+#define MAX_INFLACION 20000
+#define MIN_ASPIRACION 3000
+#define MAX_ASPIRACION 20000
+
+#define MODO_PAUSA 0
+#define MODO_INFLACION 1
+#define MODO_ASPIRACION -1
+
+int modo;
+unsigned long tiempo_ini;
+unsigned long tiempo_fin;
+
+
+void setup() {
+  // Definición de los pines
+  pinMode(PIN_INFLADOR_01, OUTPUT);
+  pinMode(PIN_INFLADOR_02, OUTPUT);
+  pinMode(PIN_INFLADOR_03, OUTPUT);
+  pinMode(PIN_INFLADOR_04, OUTPUT);
+
+  pinMode(PIN_ASPIRADOR_01, OUTPUT);
+  pinMode(PIN_ASPIRADOR_02, OUTPUT);
+  pinMode(PIN_ASPIRADOR_03, OUTPUT);
+  pinMode(PIN_ASPIRADOR_04, OUTPUT);
+
+  pinMode(A5, OUTPUT);
+
+  // Se inicia con la pausa mínima
+  modo = MODO_PAUSA;
+  tiempo_ini = millis();
+  tiempo_fin = tiempo_ini + random(MIN_PAUSA, MAX_PAUSA);
+
+  // Inicialización del puerto serial
+  Serial.begin(9600);
+  delay(1000);
+  Serial.print("Iniciando el modo PAUSA - Duración=");
+  Serial.println(tiempo_fin - tiempo_ini);
+}
+
+
+/**
+ * Ciclo principal de ejecución
+ */
+void loop() {
+
+  // Modo "PAUSA"
+  if (modo == MODO_PAUSA) {
+    digitalWrite(PIN_INFLADOR_01, LOW);
+    digitalWrite(PIN_INFLADOR_02, LOW);
+    digitalWrite(PIN_INFLADOR_03, LOW);
+    digitalWrite(PIN_INFLADOR_04, LOW);
+
+    digitalWrite(PIN_ASPIRADOR_01, LOW);
+    digitalWrite(PIN_ASPIRADOR_02, LOW);
+    digitalWrite(PIN_ASPIRADOR_03, LOW);
+    digitalWrite(PIN_ASPIRADOR_04, LOW);
+    digitalWrite(A5, LOW);
+
+    if (millis() > tiempo_fin) {
+      modo = MODO_INFLACION;
+      tiempo_ini = millis();
+      tiempo_fin = tiempo_ini + random(MIN_INFLACION, MAX_INFLACION);
+      Serial.print("Iniciando el modo INFLACIÓN - Duración=");
+      Serial.println(tiempo_fin - tiempo_ini);
+    }
+  }
+
+  // Modo "INFLACIÓN"
+  else if (modo == MODO_INFLACION) {
+    digitalWrite(PIN_INFLADOR_01, HIGH);
+    digitalWrite(PIN_INFLADOR_02, HIGH);
+    digitalWrite(PIN_INFLADOR_03, HIGH);
+    digitalWrite(PIN_INFLADOR_04, HIGH);
+
+    digitalWrite(PIN_ASPIRADOR_01, LOW);
+    digitalWrite(PIN_ASPIRADOR_02, LOW);
+    digitalWrite(PIN_ASPIRADOR_03, LOW);
+    digitalWrite(PIN_ASPIRADOR_04, LOW);
+    digitalWrite(A5, LOW);
+
+    if (millis() > tiempo_fin) {
+      modo = MODO_ASPIRACION;
+      tiempo_ini = millis();
+      tiempo_fin = tiempo_ini + random(MIN_ASPIRACION, MAX_ASPIRACION);
+      Serial.print("Iniciando el modo ASPIRACIÓN - Duración=");
+      Serial.println(tiempo_fin - tiempo_ini);
+    }
+  }
+
+  // Modo "ASPIRACION"
+  else if (modo == MODO_ASPIRACION) {
+    digitalWrite(PIN_INFLADOR_01, LOW);
+    digitalWrite(PIN_INFLADOR_02, LOW);
+    digitalWrite(PIN_INFLADOR_03, LOW);
+    digitalWrite(PIN_INFLADOR_04, LOW);
+
+    digitalWrite(PIN_ASPIRADOR_01, HIGH);
+    digitalWrite(PIN_ASPIRADOR_02, HIGH);
+    digitalWrite(PIN_ASPIRADOR_03, HIGH);
+    digitalWrite(PIN_ASPIRADOR_04, HIGH);
+    digitalWrite(A5, HIGH);
+
+    if (millis() > tiempo_fin) {
+      modo = MODO_PAUSA;
+      tiempo_ini = millis();
+      tiempo_fin = tiempo_ini + random(MIN_PAUSA, MAX_PAUSA);
+      Serial.print("Iniciando el modo PAUSA - Duración=");
+      Serial.println(tiempo_fin - tiempo_ini);
+    }
+  }
+}
