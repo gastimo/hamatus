3dd12b47a2bdcd28a680ecbf66588ea492823005
Gastón Martino <gaston.martino@gmail.com>
2026-07-24 01:06:12 -0300

    Añadido el código para el círculo "IATROS"

--- circuloIATROS/circuloIATROS.ino
+++ circuloIATROS/circuloIATROS.ino
@@ -1,11 +1,41 @@
-// Pines de Arduino que controlan las bobinas del motor
-int IN1 = 8;
-int IN2 = 9;
-int IN3 = 10;
-int IN4 = 11;
+/**
+ * Círculo: "LAS FALLAS DE IATROS"
+ * -------------------------------
+ * Código para controlar 2 motores "paso a paso" (PAP) unipolares
+ * 28BYJ, responsables de hacer girar los ejes del círculo.
+ */
+ 
+// MOTOR 1 - Definición de pines
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+int M1_IN1 = 13;
+int M1_IN2 = 12;
+int M1_IN3 = 11;
+int M1_IN4 = 10;
+
+// MOTOR 2 - Definición de pines
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+int M2_IN1 = 9;
+int M2_IN2 = 8;
+int M2_IN3 = 7;
+int M2_IN4 = 6;
+
+// MOTOR 3 - Definición de pines
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+int M3_IN1 = 5;
+int M3_IN2 = 4;
+int M3_IN3 = 3;
+int M3_IN4 = 2;
+
+// MOTOR 3 - Definición de pines
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+int M4_IN1 = A5;
+int M4_IN2 = A4;
+int M4_IN3 = A3;
+int M4_IN4 = A2;
+
 
 // Demora entre pasos, no debe ser menor a 10ms (tiempo mínimo para energizar al motor)
-int demora = 12;
+int demora = 10;
 
 
 // Matriz para realizar el "Paso Completo con Dos Bobinas"
@@ -24,10 +54,25 @@ int paso[4][4] = {
  * Definición de los pines para energizar las bobinas del motor
  */
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  pinMode(M1_IN1, OUTPUT);
+  pinMode(M1_IN2, OUTPUT);
+  pinMode(M1_IN3, OUTPUT);
+  pinMode(M1_IN4, OUTPUT);
+
+  pinMode(M2_IN1, OUTPUT);
+  pinMode(M2_IN2, OUTPUT);
+  pinMode(M2_IN3, OUTPUT);
+  pinMode(M2_IN4, OUTPUT);
+
+  pinMode(M3_IN1, OUTPUT);
+  pinMode(M3_IN2, OUTPUT);
+  pinMode(M3_IN3, OUTPUT);
+  pinMode(M3_IN4, OUTPUT);
+
+  pinMode(M4_IN1, OUTPUT);
+  pinMode(M4_IN2, OUTPUT);
+  pinMode(M4_IN3, OUTPUT);
+  pinMode(M4_IN4, OUTPUT);
 }
 
 /**
@@ -54,19 +99,30 @@ void loop() {
 void rotacionEnPasos() {
   for (int i = 0; i < 512; i++) {  // 512 * 4 (pasos del ciclo) = 2048 pasos
     for (int j = 0; j < 4; j++) {
-      digitalWrite(IN1, paso[j][0]);
-      digitalWrite(IN2, paso[j][1]);
-      digitalWrite(IN3, paso[j][2]);
-      digitalWrite(IN4, paso[j][3]);
+      digitalWrite(M1_IN1, paso[j][0]);
+      digitalWrite(M2_IN1, paso[j][0]);
+      digitalWrite(M3_IN1, paso[j][0]);
+      digitalWrite(M4_IN1, paso[j][0]);
+
+      digitalWrite(M1_IN2, paso[j][1]);
+      digitalWrite(M2_IN2, paso[j][1]);
+      digitalWrite(M3_IN2, paso[j][1]);
+      digitalWrite(M4_IN2, paso[j][1]);
+
+      digitalWrite(M1_IN3, paso[j][2]);
+      digitalWrite(M2_IN3, paso[j][2]);
+      digitalWrite(M3_IN3, paso[j][2]);
+      digitalWrite(M4_IN3, paso[j][2]);
+
+      digitalWrite(M1_IN4, paso[j][3]);
+      digitalWrite(M2_IN4, paso[j][3]);
+      digitalWrite(M3_IN4, paso[j][3]);
+      digitalWrite(M4_IN4, paso[j][3]);
+
       delay(demora);
     }
   }
 
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  delay(5000);
 }
 
 
